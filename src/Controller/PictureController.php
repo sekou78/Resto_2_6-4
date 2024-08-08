8a25682a81c282c9f9c0cbbefa7670149ad80378
sekou78 <s.cisse78@gmail.com>
2024-08-08 21:14:22 +0200

    Creation du CRUD Picture

---
+++ src/Controller/PictureController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Picture;
+use App\Form\PictureType;
+use App\Repository\PictureRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/picture')]
+class PictureController extends AbstractController
+{
+    #[Route('/', name: 'app_picture_index', methods: ['GET'])]
+    public function index(PictureRepository $pictureRepository): Response
+    {
+        return $this->render('picture/index.html.twig', [
+            'pictures' => $pictureRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_picture_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $picture = new Picture();
+        $form = $this->createForm(PictureType::class, $picture);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($picture);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_picture_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('picture/new.html.twig', [
+            'picture' => $picture,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_picture_show', methods: ['GET'])]
+    public function show(Picture $picture): Response
+    {
+        return $this->render('picture/show.html.twig', [
+            'picture' => $picture,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_picture_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Picture $picture, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(PictureType::class, $picture);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_picture_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('picture/edit.html.twig', [
+            'picture' => $picture,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_picture_delete', methods: ['POST'])]
+    public function delete(Request $request, Picture $picture, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$picture->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($picture);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_picture_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Form/PictureType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Picture;
+use App\Entity\Restaurant;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PictureType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('slug')
+            ->add('createdAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('updatedAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('restaurant', EntityType::class, [
+                'class' => Restaurant::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Picture::class,
+        ]);
+    }
+}
