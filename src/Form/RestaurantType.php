d28a75642b9904a904547b5be88f45720133a1ba
sekou78 <s.cisse78@gmail.com>
2024-08-08 21:13:08 +0200

    Creation du CRUD Restaurant

---
+++ src/Form/RestaurantType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Restaurant;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RestaurantType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('uuid')
+            ->add('name')
+            ->add('description')
+            ->add('amOpeningTime')
+            ->add('pmOpeningTime')
+            ->add('maxGuest')
+            ->add('createdAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('updatedAt', null, [
+                'widget' => 'single_text',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Restaurant::class,
+        ]);
+    }
+}
