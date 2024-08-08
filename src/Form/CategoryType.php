5ca75efd382ce0e95a729bae7025eaa816d2fa3f
sekou78 <s.cisse78@gmail.com>
2024-08-08 21:16:24 +0200

    Creation du CRUD Category

---
+++ src/Form/CategoryType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Category;
+use App\Entity\Food;
+use App\Entity\Menu;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CategoryType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('uuid')
+            ->add('title')
+            ->add('createdAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('updatedAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('menus', EntityType::class, [
+                'class' => Menu::class,
+                'choice_label' => 'id',
+                'multiple' => true,
+            ])
+            ->add('food', EntityType::class, [
+                'class' => Food::class,
+                'choice_label' => 'id',
+                'multiple' => true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Category::class,
+        ]);
+    }
+}
