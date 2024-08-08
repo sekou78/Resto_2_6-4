999da0a67864c9a088ab47cf863ffe4803f6de86
sekou78 <s.cisse78@gmail.com>
2024-08-08 21:15:00 +0200

    Creation du CRUD Menu

---
+++ src/Form/MenuType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Category;
+use App\Entity\Menu;
+use App\Entity\Restaurant;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MenuType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('uuid')
+            ->add('title')
+            ->add('description')
+            ->add('price')
+            ->add('createdAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('updatedAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('restaurant', EntityType::class, [
+                'class' => Restaurant::class,
+                'choice_label' => 'id',
+            ])
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'id',
+                'multiple' => true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Menu::class,
+        ]);
+    }
+}
