7cac2ac39f7b5373ba86726d16820525d7e6d161
sekou78 <s.cisse78@gmail.com>
2024-08-18 15:32:04 +0200

    Ajout de d'autre test des routes

--- tests/Controller/SmokeTest.php
+++ tests/Controller/SmokeTest.php
@@ -6,35 +6,35 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class SmokeTest extends WebTestCase
 {
-    public function testApiDocUrlIsSuccessful(): void
+    // public function testApiDocUrlIsSuccessful(): void
+    // {
+    //     $client = self::createClient();
+    //     $client->followRedirects(false);
+    //     $client->request("GET", "/api/doc");
+
+    //     self::assertResponseIsSuccessful();
+    // }
+
+    // public function testApiAccountUrlIsSecure(): void
+    // {
+    //     $client = self::createClient();
+    //     $client->followRedirects(false);
+    //     $client->request("GET", "/api/account/me");
+    //     // $client->request("GET", "/api/account/edit");
+
+    //     self::assertResponseStatusCodeSame(401);
+    // }
+
+    public function testLoginRouteCanConnectAValidUser(): void
     {
         $client = self::createClient();
         $client->followRedirects(false);
-        $client->request("GET", "/api/doc");
-
-        self::assertResponseIsSuccessful();
-    }
-
-    public function testApiAccountUrlIsSecure(): void
-    {
-        $client = self::createClient();
-        $client->followRedirects(false);
-        $client->request("GET", "/api/account/me");
-        // $client->request("GET", "/api/account/edit");
-
-        self::assertResponseStatusCodeSame(401);
-    }
-
-    public function testLoginRouteCanConnectValidUser(): void
-    {
-        $client = self::createClient();
-        // $client->followRedirects(false);
 
         // $client->request('POST', '/api/registration', [], [], [
         //     'Content-Type' => 'application/json',
         // ], json_encode([
-        //     "email" => "adresse@mail.com",
-        //     "password" => "mot de passe",
+        //     "email" => "toto@mail.com",
+        //     "password" => "toto",
         //     "firstname" => "Fath",
         //     "lastname" => "Dinga",
         //     "guestNumber" => 20,
@@ -45,8 +45,41 @@ class SmokeTest extends WebTestCase
             'CONTENT_TYPE' => 'application/json',
 
         ], json_encode([
-            "username" => "adresse@email.com",
-            "password" => "Mot de passe",
+            "username" => "toto@email.com",
+            "password" => "toto",
+        ], JSON_THROW_ON_ERROR));
+
+        $statusCode = $client->getResponse()->getStatusCode();
+        dd($statusCode);
+    }
+
+    public function testLoginRouteCanConnectAValidBooking(): void
+    {
+        $client = self::createClient();
+        $client->followRedirects(false);
+
+        $client->request('POST', '/api/booking', [], [], [
+            'CONTENT_TYPE' => 'application/json',
+        ], json_encode([
+            "guestNumber" => 5,
+            "orderDate" => "2024-04-17",
+            "orderHour" => "2024-04-17 16:30",
+            "allergy" => "Cacahuètes"
+        ], JSON_THROW_ON_ERROR));
+
+        $statusCode = $client->getResponse()->getStatusCode();
+        dd($statusCode);
+    }
+
+    public function testLoginRouteCanConnectAValidCategory(): void
+    {
+        $client = self::createClient();
+        $client->followRedirects(false);
+
+        $client->request('POST', '/api/category', [], [], [
+            'CONTENT_TYPE' => 'application/json',
+        ], json_encode([
+            "title" => "Titre du category",
         ], JSON_THROW_ON_ERROR));
 
         $statusCode = $client->getResponse()->getStatusCode();
